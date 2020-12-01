36dc906ac32741c210edcfc277d7f53b297b9ef6
Suguru_Ueno <sgr_ueno@outlook.com>
2020-12-01 18:51:24 +0900

    記事に関するモデルを定義

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+#お知らせ記事のモデル
+class article(models.Model):
+  title = models.CharField('タイトル', max_length=255)
+  text = models.TextField('本文')
+  pub_date = models.DateTimeField('作成日', default=timezone.now)
