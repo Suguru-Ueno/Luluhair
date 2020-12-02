83ed8fa2cff787b4c094ef96afd7ba1350f92356
Suguru_Ueno <sgr_ueno@outlook.com>
2020-12-02 14:11:45 +0900

    管理サイトから、画像付き記事の投稿確認

--- blog/models.py
+++ blog/models.py
@@ -3,8 +3,11 @@ from django.utils import timezone
 # Create your models here.
 
 #お知らせ記事のモデル
-class article(models.Model):
+class Article(models.Model):
   title = models.CharField('タイトル', max_length=255)
-  image = models.ImageField(upload_to='images',blank=True, null=True)
+  image = models.ImageField('画像', upload_to='images',blank=True, null=True)
   text = models.TextField('本文')
   pub_date = models.DateTimeField('作成日', default=timezone.now)
+
+  def __str__(self):
+    return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from .models import Article
 # Create your views here.
 def index(request):
+  articles = Article.objects.all()
   words = {
-    'msg':'確認の表示'
+    'msg':'確認の表示',
+    'articles':articles,
   }
   return render(request, 'blog/index.html', words)
